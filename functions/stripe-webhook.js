5c2e986953c8e7ad3768f6d7141a366657a2fd99
Dev Mukherjee <devraj@gmail.com>
2022-12-28 10:19:42 +1100

    feat: adds stripe webhook handler

--- functions/stripe-webhook.js
+++ functions/stripe-webhook.js
@@ -6,41 +6,41 @@ const getNetlifyUser = (netlifyId) => {
 
 exports.handler = async ({ body, headers }, context) => {
 
-    try {
+  try {
 
-        // Stripe requires you to do this to ensure that the
-        // webhook is from Stripe, if it's not this will fail
-        // and raise the exception
-        const stripeEvent = stripe.webhooks.constructEvent(
-            body,
-            headers['stripe-signature'],
-            process.env.STRIPE_WEBHOOK_SECRET,
-        );
-        
-        switch (stripeEvent.type) {
-            case 'payment_intent.succeeded':
-              const paymentIntent = stripeEvent.data.object;
-              break;
-            default:
-                return;
-        }
-
-        // Send the yay ok to Stripe which will mark it
-        // processed on their part and will never fire again
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ 
-                processed: true 
-            }),
-        };
-
+    // Stripe requires you to do this to ensure that the
+    // webhook is from Stripe, if it's not this will fail
+    // and raise the exception
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+    
+    switch (stripeEvent.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = stripeEvent.data.object;
+        break;
+      default:
+        return;
     }
-    catch (err) {
-        // This will make Stripe retry at a later time.
-        return {
-            statusCode: 400,
-            body: `Webhook Error: ${err.message}`,
-        };
-    }
+
+    // Send the yay ok to Stripe which will mark it
+    // processed on their part and will never fire again
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ 
+        processed: true 
+      }),
+    };
+
+  }
+  catch (err) {
+    // This will make Stripe retry at a later time.
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
 
 };
