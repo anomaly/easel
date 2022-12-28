bc29a77fd59a793300a5cc23c779bc1fafbd2180
Dev Mukherjee <devraj@gmail.com>
2022-12-28 11:25:41 +1100

    feat: adds netlify identity based logins
    adds the netlify identity library and adds login and logout options for the user
    this is achieved using their inline library and react hooks, see this article by
    @cassidoo on @netlify blog
    https://www.netlify.com/blog/2020/07/15/integrating-netlify-identity-into-your-next.js-apps/
    
    note: we need to refactor the location of the function to a header so it can be
    made available across the site

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
+import { useEffect, useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 
+import netlifyAuth from '../netlifyAuth.js'
+
 export default function Home() {
+
+  let [
+    loggedIn, 
+    setLoggedIn
+  ] = useState(netlifyAuth.isAuthenticated)
+
+  let [user, setUser] = useState(null)
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user)
+    })
+  }, [loggedIn])
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user)
+      setUser(user)
+      netlifyAuth.closeModal()
+    })
+  }
+  
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false)
+      setUser(null)
+    })
+  }
+
   return (
     <div>
       <h1>Hello world</h1>
+      {loggedIn ? (
+        <div>
+        You are logged in!
+        {user && <>Welcome {user?.user_metadata.full_name}!</>}
+        <br /> 
+        <button onClick={logout}>
+          Log out here.
+        </button>
+        </div>
+      ) : (
+        <button onClick={login}>
+          Log in here.
+        </button>
+      )}
     </div>
   )
 }
